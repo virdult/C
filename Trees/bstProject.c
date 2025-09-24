e6bc84b823885537c51d447284435807cb93a6d6
Virdult <virdult@gmail.com>
2025-09-24 18:44:05 +0300

    Project files added

---
+++ Trees/bstProject.c
@@ -0,0 +1,6 @@
+/*Write a function that, given a BST and a range [L, R], returns the sum of all 
+node values within that range (inclusive).
+Requirements:
+Must traverse efficiently (avoid visiting unnecessary nodes).
+Should use recursion.
+Extra: Implement a version that returns the sum without recursion.*/
---
+++ Trees/bstProject2.c
@@ -0,0 +1,5 @@
+/*Problem: Write a function that finds the Lowest Common Ancestor (LCA) of two given nodes in a BST.
+The LCA is the deepest node that is an ancestor of both nodes.
+Requirements:
+Must work in O(h) time, where h = tree height.
+Should not use extra space apart from recursion stack.*/
