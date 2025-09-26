f81d3bc849b197921e4289ecc5d6d21d11d27322
Virdult <virdult@gmail.com>
2025-09-26 08:27:16 +0300

    AVL Project start

--- Trees/avlProject.c
+++ Trees/avlProject.c
@@ -30,6 +30,10 @@ int height(Node* n){
     return (n == NULL) ? 0 : n->height;
 }
 
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
 int isBalanced(Node* n){
     if(n == NULL) return 0;
     return height(n->left) - height(n->right);
@@ -42,5 +46,105 @@ Node* rightRotate(Node* y){
     x->right = y;
     y->right = r;
 
-    x->height = height
+    x->height = 1 + max(height(x->left), height(x->right));
+    y->height = 1 + max(height(y->left), height(y->right));
+    return x;
+}
+
+Node* leftRotate(Node* x){
+    Node* y = x->right;
+    Node* l = y->left;
+
+    y->left = x;
+    x->right = l;
+
+    y->height = 1 + max(height(y->left), height(y->right));
+    x->height = 1 + max(height(x->left), height(x->right));
+
+    return y;
+}
+
+Node* insert(Node* node, int val){
+    if(node == NULL) return node;
+
+    if(val < node->data){
+        node->left = insert(node->left, val);
+    }else if(val > node->data){
+        node->right = insert(node->left, val);
+    }else{
+        node->count++;
+        return node;
+    }
+    node->height = 1 + max(height(node->left), height(node->right));
+
+    int balance = isBalanced(node);
+
+    if(balance < -1 && val > node->right->data){
+        return leftRotate(node);
+    }
+    if(balance < -1 && val < node->right->data){
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+    if(balance > 1 && val > node->left->data){
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+    if(balance > 1 && val < node->left->data){
+        return rightRotate(node);
+    }
+    return node;
+}
+
+Node* deleteNode(Node* root, int val){
+    if(root == NULL) return root;
+
+    if(val < root->data){
+        root->left = deleteNode(root->left, val);
+    }else if(val > root->data){
+        root->right = deleteNode(root->right, val);
+    }else{
+        if(root && root->count > 1){
+            root->count--;
+            return root;
+        }
+        if(root->left == NULL){
+            Node* temp = root->right;
+            free(root);
+            return temp;
+        }else if(root->right == NULL){
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+        Node* temp = root->right;
+        while(temp->left != NULL){
+            temp = temp->left;
+        }//This is basically find min in the right subtree.
+        root->count = temp->count;
+        root->data = temp->data;
+        temp->count = 1;
+        deleteNode(root->right, temp->data);
+    }
+    //Deleting completed, now time for rearrangements.
+    if(root == NULL) return root;
+
+    root->height = 1 + max(height(root->left), height(root->right));
+    int balance = isBalanced(root);
+
+    if(balance < -1 && isBalanced(root->right) <= 0){
+        return leftRotate(root);
+    }
+    if(balance < -1 && isBalanced(root->right) > 0){
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+    if(balance > 1 && isBalanced(root->left) < 0){
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+    if(balance > 1 && isBalanced(root->left) >= 0){
+        return rightRotate(root);
+    }
+    return root;
 }
